2c6b9a4591bc53cdf89d569137baced2d98cf904
Sonali <patilsonalias002@gmail.com>
2023-10-06 11:32:46 +0530

    add length

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // app.js
 // Function to fetch users from local storage or fetch from API
 function fetchUsers(callback) {
-    const storedUsers = JSON.parse(localStorage.getItem('users')) ;
-    if (!storedUsers || storedUsers.length === 0) {
+    const storedUsers = JSON.parse(localStorage.getItem('users')) || [];
+    if (storedUsers.length === 0) {
         // Simulated user data fetch function (meant to represent an API call)
         setTimeout(() => {
             const initialUsers = [
@@ -39,7 +39,7 @@ function displayUsers(users) {
 
 // Function to add a new user
 function addUser(userName) {
-    const storedUsers = JSON.parse(localStorage.getItem('users')) ;
+    const storedUsers = JSON.parse(localStorage.getItem('users'))|| [] ;
     const newUser = { id: Date.now(), name: userName };
     storedUsers.push(newUser);
     localStorage.setItem('users', JSON.stringify(storedUsers));
@@ -48,7 +48,7 @@ function addUser(userName) {
 
 // Function to delete a user by ID
 function deleteUser(userId) {
-    let storedUsers = JSON.parse(localStorage.getItem('users')) ;
+    let storedUsers = JSON.parse(localStorage.getItem('users')) || [];
     storedUsers = storedUsers.filter((user) => user.id !== userId);
     localStorage.setItem('users', JSON.stringify(storedUsers));
     fetchUsers(displayUsers);
@@ -56,7 +56,7 @@ function deleteUser(userId) {
 
 // Function to search for users by name
 function searchUsers(query) {
-    const storedUsers = JSON.parse(localStorage.getItem('users')) ;
+    const storedUsers = JSON.parse(localStorage.getItem('users')) || [];
     const filteredUsers = storedUsers.filter((user) =>
         user.name.toLowerCase().includes(query.toLowerCase())
     );
